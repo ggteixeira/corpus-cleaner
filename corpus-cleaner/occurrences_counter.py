c5dadf5baa008ee22306ae4e0defecf4dd47e52e
guiemi <guiemi.teixeira@gmail.com>
2020-04-14 21:08:35 -0300

    Adiciona um código que lista os tokens que mais aparecem no texto

---
+++ corpus-cleaner/occurrences_counter.py
@@ -0,0 +1,17 @@
+entrada = open("corpora/paradigmas_desenvolvimento.txt", "r", encoding="utf-8")
+saida = open("saida_pt.txt", "w", encoding="utf-8")
+
+lexico = {}
+for linha in entrada:
+    tokens = linha.strip().split(" ")
+    for token in tokens:
+        if token.isalpha():
+            token = token.lower()
+            lexico[token] = lexico.get(token, 0) + 1
+
+for token in sorted(lexico.keys(), key=lexico.get, reverse=True):
+    saida.write(token + ": " + str(lexico.get(token)) + "\n")
+
+
+entrada.close()
+saida.close()
