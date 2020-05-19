351bc9e105e4d63f955974fb1180832963aa2bc4
guiemi <guiemi.teixeira@gmail.com>
2020-05-19 20:15:56 -0300

    Retira código comentado excessivo e FAZ COMENTARIOS EXPLICANDO cada operação da função

--- corpus-cleaner/ngrams_cleaner.py
+++ corpus-cleaner/ngrams_cleaner.py
@@ -1,17 +1,16 @@
 # Abre o arquivo com os n-gramas:
-with open("ngrams_terminet/candidatos_backup/1_gram/CD,CiclosVidaSoftware,DevMedia.txt", "r") as file:
+with open("ngrams_terminet/candidatos/3_gram/TC,UFLA,Schneide.txt", "r") as file:
     file = file.read()
 
 # Abre o arquivo com as stopwords
-with open("stopwords/my_stopwords.txt", "r") as stopwords:
+with open("stopwords/my_stopwords_3-grams.txt", "r") as stopwords:
     stopwords = stopwords.read()
 
 
 def str_2_list(text_file):
     """Converte os itens do arquivo de n-gramas em uma lista
     """
-    converted_2_list = text_file.split(
-        '\n')  # Transforma os itens em uma lista
+    converted_2_list = text_file.split("\n")  # Transforma os itens em uma lista
     return converted_2_list
 
 
@@ -31,33 +30,24 @@ def filter_ngrams(ngram_list, stopwords_list):
     # Transforma os itens do arquivo de stopwords em uma lista:
     stopwords_list = stopwords_list.split("\n")
 
-    filtered_ngrams = list()
+    # filtered_ngrams = list()
     blacklist = list()
     blacklist_without_duplicates = list()
 
     for ngram in ngram_list:  # para cada n-grama
-        for stopword in stopwords_list:
-            if stopword in ngram:
+        for stopword in stopwords_list:  # p/ cada stopword na lista stopwords
+            if stopword in ngram:  # se o n-grama contém a stopword
 
-                blacklist.append(ngram)
-                # filtered_ngrams.append(ngram)
-                # saida.write(f"{ngram}\n")
-    # print(f"S T O P W O R D S: \n\n{stopwords_list}")
+                blacklist.append(ngram)  # adiciona o n-grama à blacklist
 
-    for i in blacklist:
-        if i not in blacklist_without_duplicates:
-            blacklist_without_duplicates.append(i)
-    # print(f"stopword_list: {stopwords_list[10:25]} (...)")
-    # print(f"ngram_list: {ngram_list}\n\n\n")
-    # print(f"Blacklist sem duplicatas: {blacklist_without_duplicates}")
+    for i in blacklist:  # p/ cada item na blacklist
+        if i not in blacklist_without_duplicates:  # se b_w_dplctes ñ contém i
+            blacklist_without_duplicates.append(i)  # add i à lista
 
-    with open("saida_ngrams.txt", "w+") as saida:
-        for ngram in ngram_list:
-            if ngram not in blacklist_without_duplicates:
-                saida.write(f"{ngram}\n")
+    with open("saida_ngrams.txt", "w+") as saida:  # abre o arquivo de saída
+        for ngram in ngram_list:  # p/ cada n-grama na lista
+            if ngram not in blacklist_without_duplicates:  # se blk_w_dplct ñ contém ngram
+                saida.write(f"{ngram}\n")  # escreve o n-grama no arquivo e vai pra próxima linha
 
-        # for i in blacklist_without_duplicates:
-            # saida.write(f"{i}\n")
-            # print(f"{i}\n")
 # Function call:
-print(filter_ngrams(list_2_set(str_2_list(file)), stopwords))
+filter_ngrams(list_2_set(str_2_list(file)), stopwords)
