37799e5e9a81d2d242156799f068a16e310934b1
Guilherme Teixeira <24235344+vibraniumdev@users.noreply.github.com>
2021-12-18 14:23:28 -0300

    Adds .gitignore

--- text_scraper.py
+++ text_scraper.py
@@ -6,13 +6,13 @@ from bs4 import BeautifulSoup
 from random import randrange
 
 
-
-def get_test_url(default_url='http://hmpg.net/'):
+def get_test_url(default_url="http://hmpg.net/"):
     test_url = input("Digite uma URL: ")
     if len(test_url) == 0:
         return default_url
     else:
         return test_url
+
 
 # Faz um request da URL e a salva numa variável
 def get_page(url=None):
@@ -32,6 +32,7 @@ def download_page(get_page):
 def html_parse(download_page):
     print(f"Tamanho do texto antes da limpeza: {len(download_page.split())} tokens")
     return BeautifulSoup(download_page, "html.parser")
+
 
 # Limpa parcialmente o conteúdo, salvando apenas texto presente na página
 def html_clean(html_parse):
@@ -55,21 +56,24 @@ def deep_clean(set_blacklist, html_clean):
             output += f"{token }\n"
 
     # Retira as pontuações do texto
-    puncts = string.punctuation.replace('-', '')
+    puncts = string.punctuation.replace("-", "")
     for token in output:
         if token in puncts:
             output = output.replace(token, "")
         if token == "\n":
             output = output.replace("\n", " ")
-    
+
     print(f"Tamanho limpo? --> {len(output.split())}")
     return output
 
+
 def write_into_file(deep_clean):
-    text_file = open(f'corpora/corpus_{randrange(0, 10000)}.txt', 'w')
+    text_file = open(f"corpora/corpus_{randrange(0, 10000)}.txt", "w")
     for token in deep_clean:
         text_file.write(token)
     text_file.close()
 
-write_into_file(deep_clean(set_blacklist, html_clean(html_parse(download_page(get_page())))))
 
+write_into_file(
+    deep_clean(set_blacklist, html_clean(html_parse(download_page(get_page()))))
+)
